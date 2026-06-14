cb4e52cfbad4206cba438235aa09690de4a309dc
slooqq <59231951+slooqq@users.noreply.github.com>
2026-06-13 23:31:38 -0500

    feat: iniciar.bat crea carpetas automaticamente

--- iniciar.bat
+++ iniciar.bat
@@ -1,4 +1,29 @@
 @echo off
 cd /d C:\repos\whatsapp-bot-estados-publicidad
+
+:: Crear carpetas de imagenes si no existen
+set "BASE=D:\PUBLICIDAD AUTONOMA"
+echo Verificando carpetas en %BASE%...
+if not exist "%BASE%" mkdir "%BASE%"
+for %%d in (
+    "1 p -  domotica"
+    "1 p -  electronica"
+    "1 p -  solar"
+    "1 p - pos pc"
+    "1 p - UNI T"
+    "1 p"
+    "2 p - domotica"
+    "2 p - electronica"
+    "2 p - solar"
+    "2 p - pos pc"
+    "2 p - u t"
+    "2 p"
+) do (
+    if not exist "%BASE%\%%~d" (
+        mkdir "%BASE%\%%~d"
+        echo   Creada: %BASE%\%%~d
+    )
+)
+
 npm start
 pause
